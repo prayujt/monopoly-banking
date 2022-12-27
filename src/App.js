2f78a931eaeb0190672771b8d9f5aa011120dcf3
Prayuj Tuli <prayujtuli@hotmail.com>
2022-12-27 13:43:52 -0500

    Added layout for property map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Col, Row } from 'antd';
 
 import Players from './components/Players';
+import PropertyMap from './components/PropertyMap'
 
 const App = () => (
 
   <div className="App">
-    <Players/>
+    <Row>
+      <Col span={8}>
+        <Players/>
+      </Col>
+      <Col span={16}>
+        <PropertyMap/>
+      </Col>
+    </Row>
   </div>
 
 );
