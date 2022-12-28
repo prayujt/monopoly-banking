7b985d96d584367a5ed9105756b75ea85ed1810b
Prayuj Tuli <prayujtuli@hotmail.com>
2022-12-28 17:05:12 -0500

    Added basic terminal, more back-end post requests

--- main.py
+++ main.py
@@ -1,4 +1,121 @@
 #!/usr/bin/env python3
 
-if __name__ == "__main__":
-    print("Hello World!")
+import requests
+import json
+
+import os
+from dotenv import dotenv_values
+
+config = {
+    **dotenv_values(),
+    **os.environ
+}
+url = config['REACT_APP_SERVER_HOST']
+
+
+def request(method, endpoint, body={}):
+    r = ''
+    if method == 'GET':
+        r = requests.get('{0}{1}'.format(url, endpoint))
+    elif method == 'POST':
+        r = requests.post('{0}{1}'.format(url, endpoint), json=body)
+    # elif method == 'PUT':
+    #     r = requests.put('{0}{1}'.format(url, endpoint), json=body)
+    elif method == 'DELETE':
+        r = requests.delete('{0}{1}'.format(url, endpoint), json=body)
+    return json.loads(r.content)
+
+
+def get_player():
+    print('Which player?')
+    players = request('GET', '/players/list')
+    for i in range(len(players)):
+        print('({0}) {1}'.format(i + 1, players[i]['name']))
+    index = input()
+
+    if index == "":
+        return None
+
+    try:
+        return players[int(index) - 1]['name']
+    except Exception:
+        print("Invalid choice!")
+        return get_player()
+
+
+def match_property():
+    keyword = input("Which property? ")
+    if keyword == "exit":
+        return None
+    properties = request('GET', '/properties/list')
+    possible_matches = []
+    for property_ in properties:
+        if property_['propName'].lower().find(keyword.lower()) != -1:
+            possible_matches.append(property_['propName'])
+
+    if len(possible_matches) == 0:
+        print("No results found! Try again or type exit to quit.")
+        return match_property()
+
+    for i in range(len(possible_matches)):
+        print('({0}) {1}'.format(i + 1, possible_matches[i]))
+
+    index = input()
+    if index == "exit":
+        return None
+    if index == "" and len(possible_matches) == 1:
+        return possible_matches[0]
+    if index == "" or int(index) <= 0 or int(index) > len(possible_matches):
+        return match_property()
+
+    return possible_matches[int(index) - 1]
+
+
+def add_players(num_players=0):
+    print('Keep inputting player names, enter a newline to exit...')
+    name = input()
+
+    while name != '':
+        request('POST', '/players/add', {'name': name})
+        name = input()
+    num_players = len(request('GET', '/players/list'))
+    if num_players < 2:
+        print('Game must have at least 2 players!')
+        add_players(num_players)
+
+
+reset = input('Do you want to reset the game? (Y/n) ')
+if reset == 'Y' or reset == 'y':
+    request('GET', '/reset')
+
+add_players()
+
+while True:
+    print('-' * 50)
+    command = input('\nEnter a command: ')
+
+    if command == 'buy':
+        property_ = match_property()
+        if property_ is None:
+            continue
+
+        player = get_player()
+        if player is None:
+            continue
+
+        result = request('POST', '/players/buy_property', {'propertyName': property_, 'name': player})
+        if result is False:
+            print("Property is already owned!")
+
+    elif command == 'trade':
+        pass
+
+    elif command == 'rent':
+        pass
+
+    elif command == 'house':
+        pass
+
+    elif command == 'go':
+        player = get_player()
+        request('POST', '/players/cross_go', {'name': player})
--- server/server.js
+++ server/server.js
@@ -12,6 +12,32 @@ app.options('/*', function (req, res) {
   res.end();
 });
 
+const changeMoneyAmountQuery = (name, amount) => {
+   return `UPDATE Players SET balance=(SELECT balance WHERE name="${name}") + ${amount} WHERE name="${name}";`;
+}
+
+const addEvent = async (event, color) => {
+  let result = await database.query(`INSERT INTO Events(event, color) VALUES("${event}", "${color}")`);
+  return result;
+}
+
+// Get Requests
+
+app.get('/reset', async (req, res) => {
+  await database.query("DELETE FROM Players;");
+  await database.query("DELETE FROM Ownership;");
+  await database.query("DELETE FROM Events;");
+  await database.query("INSERT INTO Events(event, color) VALUES(\"Game started!\", \"green\")");
+  res.set('Access-Control-Allow-Origin', '*');
+  res.send(true);
+})
+
+app.get('/properties/list', async (req, res) => {
+  let results = await database.query("SELECT * FROM Properties;");
+  res.set('Access-Control-Allow-Origin', '*');
+  res.send(results);
+})
+
 app.get('/players/list', async (req, res) => {
   let results = await database.query("SELECT * FROM Players;");
   res.set('Access-Control-Allow-Origin', '*');
@@ -25,26 +51,59 @@ app.get('/players/*/properties', async (req, res) => {
 });
 
 app.get('/events', async (req, res) => {
-  let results = await database.query(`SELECT * FROM Events ORDER BY time`);
+  let results = await database.query(`SELECT * FROM Events ORDER BY time DESC;`);
   res.set('Access-Control-Allow-Origin', '*');
   res.send(results);
 });
 
+
+// Post Requests
+
 app.post('/players/add', async (req, res) => {
-  await database.query(`INSERT INTO Players VALUES("${req.body.name}", 15000000)`);
+  await database.query(`INSERT INTO Players VALUES("${req.body.name}", 15000000);`);
   res.set('Access-Control-Allow-Origin', '*');
   res.send(true);
-})
+});
+
+app.post('/players/add', async (req, res) => {
+  await database.query(`INSERT INTO Players VALUES("${req.body.name}", 15000000);`);
+  res.set('Access-Control-Allow-Origin', '*');
+  res.send(true);
+});
+
+app.post('/players/cross_go', async (req, res) => {
+  await database.query(changeMoneyAmountQuery(req.body.name, 2000000));
+  await addEvent(`${req.body.name} crossed go!`, 'green');
+  res.set('Access-Control-Allow-Origin', '*');
+  res.send(true);
+});
+
+app.post('/players/buy_property', async (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  let row = await database.query(`SELECT price FROM Properties WHERE propName="${req.body.propertyName}"`);
+  await database.query(changeMoneyAmountQuery(req.body.name, -row[0]['price']));
+  try {
+    await database.query(`INSERT INTO Ownership VALUES("${req.body.propertyName}", "${req.body.name}");`);
+    await addEvent(`${req.body.name} bought ${req.body.propertyName} for ${row[0]['price']}!`, 'red');
+    res.send(true);
+  }
+  catch {
+    res.send(false)
+  }
+});
+
+// Delete Requests
 
 app.delete('/players/remove', async (req, res) => {
-  await database.query(`DELETE FROM Players WHERE name="${req.body.name}"`);
+  await database.query(`DELETE FROM Players WHERE name="${req.body.name}";`);
   res.set('Access-Control-Allow-Origin', '*');
   res.send(true);
-})
+});
+
 
 let server = app.listen(8000, () => {
   let host = server.address().address
   let port = server.address().port
 
   console.log("App listening at http://%s:%s", host, port)
-})
+});
